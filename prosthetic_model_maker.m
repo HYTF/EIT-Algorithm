e8e230fe04cb0bb8fee1be875a4c477abb170034
jesskai <72155880+jesskai@users.noreply.github.com>
2024-11-05 15:58:37 -0600

    Add files via upload

--- prosthetic_model_maker.m
+++ prosthetic_model_maker.m
@@ -1,28 +1,31 @@
 %msh sets max size of mesh pieces
-msh=1;
+msh=3;
 %let's do a quick calculation for electrode position
 %the format we're giving to netgen is a matrix of [angle, z; angle2, z2]
 %all distances in cm
-angles = linspace(0,360,6);
+%nelec is the number of electrodes per row
+nelec = 19;
+angles = linspace(0,360,19+1);
 angles = angles(1:end-1);
-contactnumber=10;
+contactnumber=38;
 elecpositions = [];
-bottom = 5;
-for z=[bottom+3, bottom + 6]
+bottom = 0;
+%REPLACE DIST1 WITH DISTANCE BTWN BOTTOM OF TANK AND BOTTOM ROW OF
+%ELECTRODES, DIST2 IS DISTANCE BETWEEN BOTTOM OF TANK AND SECOND TO BOTTOM
+%ROW OF ELECTRODES
+dist1 = 3;
+dist2 = 6;
+for z=[dist1, dist2]
     for angle=angles
-        if z ==bottom+3
-            elecpositions = [elecpositions;angle+(180/5),z];
-        else
-            elecpositions = [elecpositions;angle,z];
-        end
+        elecpositions = [elecpositions;angle,z];
     end
 
 end
 %let's set the other variables
 %elec_ are electrode variables, elec_msh is max size of mesh elems in
 %electrode
-height = 13.5;
-radius = 35/(2*pi);
+height = 30;
+radius = 14;
 % elec_shape = [width,height, maxsz]  % Rectangular elecs
 %      OR
 % elec_shape = [radius, 0, maxsz ]    % Circular elecs
@@ -36,6 +39,8 @@ prosimg = mk_image(fmdl,1); %calculated S/m
 proszersins = ZernSinPolinpPar2(fmdl, height, radius, contactnumber, 3, 2);
 JGQ=[];
 allmeas = allmeasgetter(contactnumber);
+%TODO: write code to take a random quarter of the measurements (every fourth row
+%of allmeas), and olug that into allmeas in the line below
 prosimg.fwd_model.stimulation = stim_meas_list(allmeas,contactnumber); %Set the measurements in EIDORS
 Jsub=calc_jacobian(prosimg); %Calculate the mesh sensitivity vectors for the measurement slice
 JGsub=Jsub*proszersins; %Map to polynomial model space
